1161d7d9701a7b2ffb6d22c592b99dff57cbbc27
ludmila-almiron <ludmialmi@gmail.com>
2026-09-19 17:11:33 -0300

    agrego carpeta database con schema sql

---
+++ database/schema.sql
@@ -0,0 +1,57 @@
+CREATE TABLE localidades (
+  id SERIAL PRIMARY KEY,
+  nombre VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE empresas (
+  id SERIAL PRIMARY KEY,
+  nombre VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE viajes (
+  id SERIAL PRIMARY KEY,
+  empresa_id INTEGER REFERENCES empresas(id),
+  tipo_dia VARCHAR(20) NOT NULL DEFAULT 'todos'
+);
+
+CREATE TABLE paradas (
+  id SERIAL PRIMARY KEY,
+  viaje_id INTEGER REFERENCES viajes(id),
+  localidad_id INTEGER REFERENCES localidades(id),
+  hora TIME NOT NULL
+);
+
+INSERT INTO localidades (nombre) VALUES
+('Posadas'),
+('Garupá'),
+('Santo Pipó'),
+('Gobernador Roca'),
+('San Ignacio'),
+('Santa Ana'),
+('Candelaria'),
+('Jardín América'),
+('Puerto Leoni'),
+('Capioví'),
+('Puerto Rico'),
+('Garuhapé'),
+('Caraguatay'),
+('María Magdalena'),
+('Montecarlo'),
+('Puerto Piray'),
+('Colonia Victoria'),
+('Eldorado'),
+('Puerto Delicia'),
+('Puerto Esperanza'),
+('Wanda'),
+('Puerto Libertad'),
+('Puerto Iguazú');
+
+INSERT INTO empresas (nombre) VALUES
+('Kruse'),
+('Río Uruguay'),
+('Empresa Argentina'),
+('Crucero del Norte'),
+('Horianski'),
+('El Cometa'),
+('Oro Verde'),
+('Expreso A. Del Valle');
